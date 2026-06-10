9d57a50f7ed3b76167d3f24facebfac983bad3ba
ViktorF <vfidler111@gmail.com>
2026-06-10 20:30:33 +0300

    add basic inspector ui

--- packages/game/game-ui/src/ui/Board.tsx
+++ packages/game/game-ui/src/ui/Board.tsx
@@ -1,26 +1,29 @@
 import { useAppSelector } from '../store/hooks';
 import Player from './Player/Player';
 import Inspector from './inspector/Inspector';
+import { CardInspectorProvider } from './inspector/CardInspectorProvider';
 
 export function Board() {
   const playerIds = useAppSelector((s) => Object.keys(s.board.players));
 
   // todo раскладка под 1/2/4 игроков, сейчас просто колонка
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 24,
-        padding: 16,
-        fontFamily: 'sans-serif',
-      }}
-    >
-      {playerIds.map((id) => (
-        <Player key={id} playerId={id} />
-      ))}
+    <CardInspectorProvider>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 24,
+          padding: 16,
+          fontFamily: 'sans-serif',
+        }}
+      >
+        {playerIds.map((id) => (
+          <Player key={id} playerId={id} />
+        ))}
 
-      <Inspector />
-    </div>
+        <Inspector />
+      </div>
+    </CardInspectorProvider>
   );
 }
